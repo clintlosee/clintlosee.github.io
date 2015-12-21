cb957faedb9b09a809d04ed6aa3e42dbe0e34844
Clint Losee <clint.losee@gmail.com>
2015-12-21 14:11:32 -0700

    Set fixed home page BG and add parallax to BG.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,8 @@ $(function() {
     var offset = 300,
         offset_opacity = 1200,
         scroll_top_duration = 700,
-        $back_to_top = $('.cl-top');
+        $back_to_top = $('.cl-top'),
+        $window = $(window);
 
     // Hide-Show back-to-top link
     $(window).scroll(function(){
@@ -18,5 +19,22 @@ $(function() {
         $('body, html').animate({
             scrollTop: 0,
             }, scroll_top_duration);
+    });
+    
+    // Parallax BG effect
+    $('section[data-type="background"]').each(function() {
+        var $bgobj = $(this);
+
+        $window.scroll(function() {
+            // scroll the bg at var speed
+            // the yPos is a negative value because we're scrolling it up
+            var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+
+            // Put together final bg position
+            var coords = '50%' + yPos + 'px';
+
+            // Move the bg
+            $bgobj.css({ backgroundPosition: coords });
+        });
     });
 });
