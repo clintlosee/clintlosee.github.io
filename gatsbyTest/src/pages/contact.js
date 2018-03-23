11ab47a6714fe6379da499577b89a9f0b281e5a0
Clint Losee <clint.losee@rcwilley.com>
2018-03-23 15:21:08 -0600

    Modifications to contact page styling

--- gatsbyTest/src/pages/contact.js
+++ gatsbyTest/src/pages/contact.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import Link from 'gatsby-link';
-import { Message, Button, Input, Container, Form } from 'semantic-ui-react';
+import { Header, Message, Button, Input, Container, Form, Segment, Divider } from 'semantic-ui-react';
+
+const styles = {
+    container: {
+        marginTop: 20,
+        marginBottom: 40
+    },
+    message: {
+        marginTop: 40,
+        marginBottom: 40
+    },
+    formStyle: {
+        marginBottom: 20
+    }
+};
 
 export default class ContactPage extends Component {
     state = { success: false };
@@ -9,24 +23,48 @@ export default class ContactPage extends Component {
 
     render() {
         return (
-            <Container >
-                <h1>Contact Me</h1>
-                <p>Send me a message:</p>
+            <Container style={styles.container}>
+                <Header size="huge">Contact Me</Header>
+                <Divider horizontal style={styles.message}>
+                    Send Me A Message:
+                </Divider>
 
-                <Form
-                    action="https://send.pageclip.co/3oMr9mtH0kSj1HIHiuD8YAe4zxxOIyjG"
-                    method="post"
-                    className="pageclip-form"
-                    onSubmit={this.handleSubmit}
-                >
-                    <Form.Group widths='equal'>
-                        <Form.Input size='large' type='text' name='name' fluid label='Name' placeholder="What's your name?" />
-                        <Form.Input size='large' type='text' name='email' fluid label='Email' placeholder='email@gmail.com' />
-                    </Form.Group>
-                    <Form.TextArea size='large' type='text' name='message' label='About' placeholder='Tell us more about you...' />
-                    <Form.Button type='submit' content='Submit' />
-                </Form>
+                <Segment raised padded color="orange" style={styles.formStyle}>
+                    <Form
+                        action="https://send.pageclip.co/3oMr9mtH0kSj1HIHiuD8YAe4zxxOIyjG"
+                        method="post"
+                        className="pageclip-form"
+                        onSubmit={this.handleSubmit}
+                    >
+                        <Form.Group widths="equal">
+                            <Form.Input
+                                size="large"
+                                type="text"
+                                name="name"
+                                fluid
+                                label="Name"
+                                placeholder="What's your name?"
+                            />
+                            <Form.Input
+                                size="large"
+                                type="text"
+                                name="email"
+                                fluid
+                                label="Email"
+                                placeholder="email@gmail.com"
+                            />
+                        </Form.Group>
+                        <Form.TextArea
+                            size="large"
+                            type="text"
+                            name="message"
+                            label="Message"
+                            placeholder="What can I help you with?"
+                        />
+                        <Form.Button type="submit" content="Submit" />
+                    </Form>
+                </Segment>
             </Container>
-        )
+        );
     }
 }
