2daa309c275280fac3ca2d44e2748c9a797d4e6e
Clint Losee <clint.losee@rcwilley.com>
2018-03-27 09:32:38 -0600

    Adjustments to prevent build fails

--- gatsby-node.js
+++
@@ -1,8 +0,0 @@
-exports.modifyWebpackConfig = ({ config, stage }) => {
-    if (stage === 'build-html') {
-        config.loader('null', {
-            test: /bad-module/,
-            loader: 'null-loader'
-        });
-    }
-};
